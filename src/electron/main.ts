c91ea725f9569cb68fc89a2c2b0e2ae288ef917d
CheaSophannarith <sophannarith38@gmail.com>
2025-09-27 11:28:25 +0700

    feat: add electron-builder configuration and utility function for environment check

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,8 +1,13 @@
 import { app, BrowserWindow} from 'electron';
 import path from 'path';
+import { isDev } from './util';
 
 app.on("ready", () => {
   const mainWindow = new BrowserWindow({});
-  mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+  if (isDev()) {
+    mainWindow.loadURL("http://localhost:5123" /* dev server url */);
+  } else {
+    mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+  }
 });
 
